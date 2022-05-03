7067d04d65209c3de202175cf93eb9957ba44695
Vlad Voznitsa <voznitsa98@gmail.com>
2022-05-03 14:36:18 +0600

    UPDATED v9

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,21 +3,14 @@ import type { AppProps } from 'next/app'
 import '../styles/fonts/fonts.css'
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { UserProvider } from '../auth/userContext'
-import { useEffect } from 'react';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000/graphql',
   cache: new InMemoryCache(),
-  // headers: {
-  //   auth: localStorage.getItem('token') || "" 
-  // }
 });
 
 function MyApp({ Component, pageProps }: AppProps) {
 
-  // useEffect(() => {
-    
-  // }, [])
   return (
     <ApolloProvider client={client}>
       <UserProvider>
