7942621001212c4a1b47f46cb310338910400629
Vlad Voznitsa <voznitsa98@gmail.com>
2022-08-02 01:03:42 +0600

    Fixed fonts

--- pages/registration.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next'
-import RegistrationComponent from '../components/Registration/Registration.component'
-
-const Registration: NextPage = () => {
-  return <RegistrationComponent/>  
-}
-
-export default Registration
