0f32741ba9b6da73d879ad849f6dcc6f3b20d33e
Vlad Voznitsa <voznitsa98@gmail.com>
2022-08-02 00:52:58 +0600

    Fixed fonts

--- components/Profile/AdminProfile.tsx
+++ components/Profile/AdminProfile.tsx
@@ -1,57 +1,57 @@
 import Image from "next/image"
 import { AdminContainer, FieldContainer, FieldLabel,FieldAmount,FieldRight, WithdrawContainer } from "./AdminProfile.styled"
 import {Button} from '../../styles/Components.styled'
-import { checkBalanceDAI, checkBalanceORBM, withdrawDAI, withdrawORBM } from "../../web3/web3Utils";
+// import { checkBalanceDAI, checkBalanceORBM, withdrawDAI, withdrawORBM } from "../../web3/web3Utils";
 import { useEffect, useState } from "react";
 
 
-interface IWithdrawComponent {
-    title: string;
-    amount: string;
-    image: string;
-    callbackFunc: () => void
-}
-const WithdrawComponent = (props: IWithdrawComponent) => {
-    return (
-        <WithdrawContainer>
-            <FieldContainer>
-                <FieldLabel>{props.title}</FieldLabel>
-                <FieldRight>
-                    <FieldAmount>{props.amount}</FieldAmount>
-                    <Image src={props.image} width={40} height={40}/>
-                </FieldRight>
-            </FieldContainer>
+// interface IWithdrawComponent {
+//     title: string;
+//     amount: string;
+//     image: string;
+//     callbackFunc: () => void
+// }
+// const WithdrawComponent = (props: IWithdrawComponent) => {
+//     return (
+//         <WithdrawContainer>
+//             <FieldContainer>
+//                 <FieldLabel>{props.title}</FieldLabel>
+//                 <FieldRight>
+//                     <FieldAmount>{props.amount}</FieldAmount>
+//                     <Image src={props.image} width={40} height={40}/>
+//                 </FieldRight>
+//             </FieldContainer>
 
-            <Button width="300px" height="80px"
-                onClick={() => {
-                    props.callbackFunc()
-                }}
-            >WITHDRAW</Button>
-        </WithdrawContainer>
-    )
-}
+//             <Button width="300px" height="80px"
+//                 onClick={() => {
+//                     props.callbackFunc()
+//                 }}
+//             >WITHDRAW</Button>
+//         </WithdrawContainer>
+//     )
+// }
 
-const AdminProfile: React.FC = () => {
-    const [DAI, setDAI] = useState("0")
-    const [ORBM, setORBM] = useState("0")
-    useEffect(() => {
-        checkBalanceDAI().then((result) => {
-            const daiBalance = result?.toString()
-            if(daiBalance) setDAI(daiBalance)
-        })
+// const AdminProfile: React.FC = () => {
+//     const [DAI, setDAI] = useState("0")
+//     const [ORBM, setORBM] = useState("0")
+//     useEffect(() => {
+//         checkBalanceDAI().then((result) => {
+//             const daiBalance = result?.toString()
+//             if(daiBalance) setDAI(daiBalance)
+//         })
 
-        checkBalanceORBM().then((result) => {
-            const orbmBalance = result?.toString()
-            if(orbmBalance) setORBM(orbmBalance)
-        })
-    }, [])
+//         checkBalanceORBM().then((result) => {
+//             const orbmBalance = result?.toString()
+//             if(orbmBalance) setORBM(orbmBalance)
+//         })
+//     }, [])
     
-    return(
-      <AdminContainer>
-          <WithdrawComponent title="DAI AVAILABLE:" amount={DAI} image="/DAI.svg" callbackFunc={withdrawDAI}/>
-          <WithdrawComponent title="ORBM AVAILABLE:" amount={ORBM} image="/ORB.svg" callbackFunc={withdrawORBM}/>
-      </AdminContainer>
-    )
-}
+//     return(
+//       <AdminContainer>
+//           <WithdrawComponent title="DAI AVAILABLE:" amount={DAI} image="/DAI.svg" callbackFunc={withdrawDAI}/>
+//           <WithdrawComponent title="ORBM AVAILABLE:" amount={ORBM} image="/ORB.svg" callbackFunc={withdrawORBM}/>
+//       </AdminContainer>
+//     )
+// }
 
-export default AdminProfile
+// export default AdminProfile
