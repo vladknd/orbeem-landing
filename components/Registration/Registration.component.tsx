9476a3616fffebd75efa3ee41ecd6b4f54a50af3
Vlad Voznitsa <voznitsa98@gmail.com>
2022-08-02 00:54:50 +0600

    Fixed fonts

--- components/Registration/Registration.component.tsx
+++ components/Registration/Registration.component.tsx
@@ -9,7 +9,7 @@ import { useRouter } from 'next/router'
 import { useMutation } from '@apollo/client'
 //#------------------LOCAL-IMPORTS-------------------#
 import { Button } from '../../styles/Components.styled'
-import { getAccount } from '../../web3/web3Utils'
+// import { getAccount } from '../../web3/web3Utils'
 import { 
     RegistrationBackground,
     RegistrationModal,
@@ -53,53 +53,53 @@ const InputField = ({label, name, setter}: InputProps) => {
 }
 
 //--------------------------------------REGISTRATION-COMPONENT:
-const RegistrationComponent = () => {
-  const Router = useRouter()
-  const [regData, SetRegData] = useState<RegData | null>(null)
+// const RegistrationComponent = () => {
+//   const Router = useRouter()
+//   const [regData, SetRegData] = useState<RegData | null>(null)
   
-  useEffect(() => {
-    setAddress()
-  }, [])
+//   useEffect(() => {
+//     setAddress()
+//   }, [])
 
   
-  const setAddress = async () => {
-    const addr = await getAccount()
-    SetRegData({publicAddress: addr})
-  }
+//   const setAddress = async () => {
+//     const addr = await getAccount()
+//     SetRegData({publicAddress: addr})
+//   }
 
-  const [signUp] = useMutation(REGISTER_USER)
-  return (
-    <RegistrationBackground>
-        <RegistrationModal>
-            <ModalHeader>
-              REGISTRATION
-            </ModalHeader>
+//   const [signUp] = useMutation(REGISTER_USER)
+//   return (
+//     <RegistrationBackground>
+//         <RegistrationModal>
+//             <ModalHeader>
+//               REGISTRATION
+//             </ModalHeader>
 
-            <RegistrationForm onSubmit={async (event) => {
-              event.preventDefault()
-              const newUser = await signUp({variables: {
-                  publicAddress: regData?.publicAddress,
-                  username: regData?.username,
-                  email: regData?.email,
-                  firstName: regData?.firstName,
-                  surname: regData?.surname
-              }})
-              console.log("NEW USER", newUser);
-              Router.push("/profile")
-            }}>
-              <InputField label="Username" name="username" setter={SetRegData}/>
-              <InputField label="Email" name="email" setter={SetRegData}/>
-              <InputField label="First Name" name="firstName" setter={SetRegData}/>
-              <InputField label="Surname" name="surname" setter={SetRegData}/>
+//             <RegistrationForm onSubmit={async (event) => {
+//               event.preventDefault()
+//               const newUser = await signUp({variables: {
+//                   publicAddress: regData?.publicAddress,
+//                   username: regData?.username,
+//                   email: regData?.email,
+//                   firstName: regData?.firstName,
+//                   surname: regData?.surname
+//               }})
+//               console.log("NEW USER", newUser);
+//               Router.push("/profile")
+//             }}>
+//               <InputField label="Username" name="username" setter={SetRegData}/>
+//               <InputField label="Email" name="email" setter={SetRegData}/>
+//               <InputField label="First Name" name="firstName" setter={SetRegData}/>
+//               <InputField label="Surname" name="surname" setter={SetRegData}/>
               
-              <Button width="250px" height="70px" mt="20px" type="submit">
-                SIGN UP
-              </Button>
-            </RegistrationForm>
+//               <Button width="250px" height="70px" mt="20px" type="submit">
+//                 SIGN UP
+//               </Button>
+//             </RegistrationForm>
             
-        </RegistrationModal>
-    </RegistrationBackground>
-  )
-}
+//         </RegistrationModal>
+//     </RegistrationBackground>
+//   )
+// }
 
-export default RegistrationComponent
+// export default RegistrationComponent
